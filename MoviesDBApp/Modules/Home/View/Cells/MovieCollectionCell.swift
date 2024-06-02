61a6b2d33b83c49f2d45b583e6a9992f8b273900
Vinod Gupta <vkgupta857@gmail.com>
2024-06-02 17:24:37 +0530

    add: rating bar in movie cell

--- MoviesDBApp/Modules/Home/View/Cells/MovieCollectionCell.swift
+++ MoviesDBApp/Modules/Home/View/Cells/MovieCollectionCell.swift
@@ -16,6 +16,9 @@ class MovieCollectionCell: UICollectionViewCell, Reusable {
 
     @IBOutlet weak var containerView: UIView!
     @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var ratingContainerView: UIView!
+    @IBOutlet weak var ratingLbl: UILabel!
+    @IBOutlet weak var ratingBar: CircularProgressView!
     @IBOutlet weak var titleLbl: UILabel!
     @IBOutlet weak var yearLbl: UILabel!
     
@@ -25,6 +28,7 @@ class MovieCollectionCell: UICollectionViewCell, Reusable {
         super.awakeFromNib()
         // Initialization code
         imageView.layer.cornerRadius = 8
+        ratingContainerView.layer.cornerRadius = ratingContainerView.frame.height / 2
         
         let longPressRecognizer = UILongPressGestureRecognizer(target: self, action: #selector(longPressed))
         longPressRecognizer.minimumPressDuration = 0.5
@@ -42,6 +46,18 @@ class MovieCollectionCell: UICollectionViewCell, Reusable {
         if let posterPath = movie.posterPath, let urlString = (NetworkManager.shared.imageBaseUrl + posterPath).addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed), let url = URL(string: urlString) {
             imageView.sd_setImage(with: url, placeholderImage: UIImage(systemName: "photo"))
         }
+        if let voteAverage = movie.voteAverage {
+            var ratingTintColor = UIColor.systemGreen
+            if voteAverage < 7 {
+                ratingTintColor = .systemYellow
+            }
+            ratingBar.trackColor = ratingTintColor.withAlphaComponent(0.2)
+            ratingBar.progressColor = ratingTintColor
+            ratingBar.setProgressWithAnimation(duration: 0, value: voteAverage / 10)
+            ratingLbl.text = String(format: "%.2f", voteAverage)
+        } else {
+            ratingContainerView.isHidden = true
+        }
         
         titleLbl.text = movie.title
         yearLbl.text = "\(movie.releaseDate?.getDate()?.getYear() ?? 0)"
@@ -49,6 +65,7 @@ class MovieCollectionCell: UICollectionViewCell, Reusable {
     
     func configureCell(playlist: Playlist) {
         imageView.image = UIImage(systemName: "play.square.stack")
+        ratingContainerView.isHidden = true
         titleLbl.text = playlist.name
         titleLbl.textAlignment = .center
         yearLbl.isHidden = true
